b727be358bb4652266cb93682d5aa1d610a6c202
Slimegamer550 <joel.elias.haverinen@gmail.com>
2021-02-11 18:13:01 +0200

    ground made

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    private CharacterController controller;
+    private float gravity = -9.81f;
+
+    private float moveSpeed = 8f;
+    private float jumpHeight = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
