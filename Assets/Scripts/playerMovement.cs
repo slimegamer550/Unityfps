dce2bf17e53e8a553406fdc9b3e3fc0b4ca9572a
Slimegamer550 <joel.elias.haverinen@gmail.com>
2021-02-18 18:12:15 +0200

    movement created

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -10,6 +10,16 @@ public class playerMovement : MonoBehaviour
     private float moveSpeed = 8f;
     private float jumpHeight = 3f;
 
+    public Transform groundCheck;
+    public float groundDistance = 0.1f;
+
+    private Vector3 move;
+    private Vector3 velocity;
+
+    public LayerMask groundMask;
+
+    private bool isGrounded;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +29,28 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        float xAxis = Input.GetAxis("Horizontal");
+        float zAxis = Input.GetAxis("Vertical");
+
+        move = transform.right * xAxis + transform.forward * zAxis;
+
+        controller.Move(move * moveSpeed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundDistance);
+
     }
 }
